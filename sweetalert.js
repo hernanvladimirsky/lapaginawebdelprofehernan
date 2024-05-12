844f21c3036ad974aa74e2ea7ddd680a0ec074c1
hernanvladimirsky <32006224+hernanvladimirsky@users.noreply.github.com>
2024-05-12 12:00:22 -0300

    Add files via upload

---
+++ sweetalert.js
@@ -0,0 +1,13 @@
+Swal.fire({
+  position: 'top',
+    title: 'Felices Vacaciones!',
+    /* html:  "<b> Cens 473 </b> Entregar Actividad 1 <b> completa </b> para el Lunes 26/06 por favor. <br> <br> Enviar actividad a: hernanvladimirsky.prof@gmail.com con asunto: Apellido_Nombre Actividad 1 Cens 473",*/
+    showClass: {
+      popup: 'animate__animated animate__fadeInDown'
+    },
+    hideClass: {
+      popup: 'animate__animated animate__fadeOutUp'
+    }
+  })
+
+  
